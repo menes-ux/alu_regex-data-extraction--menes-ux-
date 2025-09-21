064c23fc697237bc15c608d8e63c12a7ed33f3e8
menes-ux <m.adisso@alustudent.com>
2025-09-21 14:30:23 +0200

    Defining the constant variable (Id's + Class's)

---
+++ script.js
@@ -0,0 +1,21 @@
+// First of all I need to get the references from the forms by defining them into constant variables using:
+
+// id's (for the element)
+const emailInput = document.getElementById("email")
+const urlInput = document.getElementById("url")
+const phoneInput = document.getElementById("phone")
+const creditcardInput = document.getElementById("credit")
+const currencyInput = document.getElementById("currency")
+
+// classes (especially for the form, to run the validation)
+const formValidation = document.querySelector(".container")
+
+// Next I will store the appropriate regex validation into varible for input validation
+const emailRegex = ;
+const urlRegex = ;
+const phoneRegex = ;
+const creditcardRegex = ;
+const currencyRegex = ;
+
+
+
